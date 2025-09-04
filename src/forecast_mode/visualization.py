77a3f750070962a8b4dc18c50a4b18d3fe217a1d
beiming tang <beiming_tang@beimings-MacBook-Air.local>
2025-09-04 00:59:00 -0400

    get details

--- src/forecast_mode/visualization.py
+++ src/forecast_mode/visualization.py
@@ -8,6 +8,11 @@ class Visualization:
 
    def plot_timeseries(self, df, x_col, y_col, title=None):
       plt.figure(figsize=(10,5))
+      plt.plot(df[x_col],df[y_col])
+      plt.title(title or f"{y_col} over {x_col})
+      plt.xlabel(x_col)
+      plt.ylabel(y_col)
+      plt.show()
 
 
 
