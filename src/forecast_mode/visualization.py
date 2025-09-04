ae21b358f7ad208a68d33883c83e56c3b743be6e
beiming tang <beiming_tang@beimings-MacBook-Air.local>
2025-09-04 00:56:11 -0400

    init visualize

--- src/forecast_mode/visualization.py
+++ src/forecast_mode/visualization.py
@@ -1,1 +1,13 @@
-hahah
+# Import Library
+import matplotlib.pyplot as plt
+
+# Define Class and Functions
+class Visualization:
+   def __init__(self, config):
+      self.config = config
+
+   def plot_timeseries(self, df, x_col, y_col, title=None):
+      plt.figure(figsize=(10,5))
+
+
+
