145b9fbae87ebb411602d80523cd7d1a8534af8f
beiming tang <beiming_tang@beimings-MacBook-Air.local>
2025-09-04 01:11:01 -0400

    intent

--- src/forecast_mode/visualization.py
+++ src/forecast_mode/visualization.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 
 # Define Class and Functions
 class Visualization:
-   def __init__(self, config):
-      self.config = config
+    def __init__(self, config):
+        self.config = config
 
-   def plot_timeseries(self, df, x_col, y_col, title=None):
-      plt.figure(figsize=(10,5))
-      plt.plot(df[x_col],df[y_col])
-      plt.title(title or f"{y_col} over {x_col})
-      plt.xlabel(x_col)
-      plt.ylabel(y_col)
-      plt.show()
+    def plot_timeseries(self, df, x_col, y_col, title=None):
+        plt.figure(figsize=(10,5))
+        plt.plot(df[x_col],df[y_col])
+        plt.title(title or f"{y_col} over {x_col})
+        plt.xlabel(x_col)
+        plt.ylabel(y_col)
+        plt.show()
 
 
 
