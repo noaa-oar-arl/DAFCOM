b5af39ccb1a39b380d116113a0d84da77fe83728
Beiming Tang <51177339+btang1@users.noreply.github.com>
2025-10-24 16:19:22 -0400

    add meteo and chem

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-
+'''
+0) import library
+'''
 from src.forecast_mode.config import load_config
 from src.forecast_mode.utils import Regrid
 from src.forecast_mode.dataloader import Load_AOD
+from src.forecast_mode.dataloader import Load_Meteo
+from src.forecast_mode.dataloader import Load_Chem
+
+import time
 
 '''
 1) load config yaml file
 '''
 config =load_config()
-
+start = time.time()
 '''
 2) load model data
 '''
@@ -19,11 +25,23 @@ ufs_meteo_folder=config['database']['dynamic_inputs']['meteorology']
 year_list = config['Time']['year_list']
 month_list = config['Time']['month_list']
 day_list = config['Time']['day_list']
-OriSize_lat_aod = config['Original_Size']['model']['dynamic_inputs']['aod'][0]
-OriSize_lon_aod = config['Original_Size']['model']['dynamic_inputs']['aod'][1]
+OriSize_lat_ufs = config['Original_Size']['model']['dynamic_inputs']['ufs'][0]
+OriSize_lon_ufs = config['Original_Size']['model']['dynamic_inputs']['ufs'][1]
 
 
-AOD_dataset = Load_AOD.extract_data(ufs_aod_folder, year_list, month_list, day_list, OriSize_lat_aod, OriSize_lon_aod)
+AOD_dataset = Load_AOD.extract_data(ufs_aod_folder, year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+METEO_SP_dataset = Load_Meteo.extract_data(ufs_meteo_folder, 'surface_pressure', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+METEO_T2M_dataset = Load_Meteo.extract_data(ufs_meteo_folder, 'temperature_2m', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+METEO_RH_dataset = Load_Meteo.extract_data(ufs_meteo_folder, 'humidity', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+METEO_EVP_dataset = Load_Meteo.extract_data(ufs_meteo_folder, 'evaporation', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+METEO_WINDU_dataset = Load_Meteo.extract_data(ufs_meteo_folder, 'wind speed u', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+METEO_WINDV_dataset = Load_Meteo.extract_data(ufs_meteo_folder, 'wind speed v', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+METEO_PBLH_dataset = Load_Meteo.extract_data(ufs_meteo_folder, 'pblh', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+METEO_PREP_dataset = Load_Meteo.extract_data(ufs_meteo_folder, 'precipitation', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+CHEM_PM25_dataset = Load_Chem.extract_data(ufs_chem_folder, 'pm25', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+CHEM_O3_dataset = Load_Chem.extract_data(ufs_chem_folder, 'o3', year_list, month_list, day_list, OriSize_lat_ufs, OriSize_lon_ufs)
+print('finish part 2')
+print(f"Step 2 done, elapsed: {time.time() - start:.2f}s")
 
 '''
 3) regrid
@@ -39,11 +57,18 @@ interpolation_method = config['interpolation_method']
 
 
 final_aod = Regrid(AOD_dataset, 'aod', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
-
-
-
-
-
+final_meteo_sp = Regrid(METEO_SP_dataset, 'surface_pressure', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_meteo_t2m = Regrid(METEO_T2M_dataset, 'temperature_2m', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_meteo_rh = Regrid(METEO_RH_dataset, 'humidity', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_meteo_evp = Regrid(METEO_EVP_dataset, 'evaporation', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_meteo_wind_u = Regrid(METEO_WINDU_dataset, 'wind speed u', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_meteo_wind_v = Regrid(METEO_WINDV_dataset, 'wind speed v', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_meteo_pblh = Regrid(METEO_PBLH_dataset, 'pblh', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_meteo_prep = Regrid(METEO_PREP_dataset, 'precipitation', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_chem_pm25 = Regrid(CHEM_PM25_dataset, 'pm25', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+final_chem_o3 = Regrid(CHEM_O3_dataset, 'o3', ur_lat, ll_lat, ur_lon, ll_lon, resolution, interpolation_method)
+print('finish part 3')
+print(f"Step 3 done, elapsed: {time.time() - start:.2f}s")
 
 
 
--- src/forecast_mode/dataloader.py
+++ src/forecast_mode/dataloader.py
@@ -88,24 +88,48 @@ class Load_Meteo:
                         with Dataset(dir_data + filelist[i], 'r') as f:
                             lat = f.variables['lat'][:]
                             lon = f.variables['lon'][:] - 360
-                            surface_pressure= f.variables['pressfc'][:][0] #unit = pa
-                            t2m_w = f.variables['tmp2m'][:][0] #unit = K
-                            specific_humidity_2m_w = f.variables['spfh2m'][:][0]  #unit = kg/kg-air
-                            evaporation_w = f.variables['evbs_ave'][:][0]+ f.variables['evcw_ave'][:][0]    #unit = W/m^2
-                            u10_w = f.variables['ugrd10m'][:][0]  #unit = m/s
-                            v10_w = f.variables['vgrd10m'][:][0]  #unit = m/s
-                            pblh_w = f.variables['hpbl'][:][0]    #unit = m
-                            precipitation_w = f.variables['tprcp'][:][0]  #unit = kg/m^2
+                            
+                            if variable_name == 'surface_pressure':
+                                surface_pressure= f.variables['pressfc'][:][0] #unit = pa
+                                all_data.append(surface_pressure)
+                                
+                            elif variable_name == 'temperature_2m':
+                                t2m_w = f.variables['tmp2m'][:][0] #unit = K
+                                all_data.append(t2m_w)
+                                
+                            elif variable_name == 'humidity':
+                                specific_humidity_2m_w = f.variables['spfh2m'][:][0]  #unit = kg/kg-air
+                                all_data.append(specific_humidity_2m_w)
+                                
+                            elif variable_name == 'evaporation':
+                                evaporation_w = f.variables['evbs_ave'][:][0]+ f.variables['evcw_ave'][:][0]    #unit = W/m^2
+                                all_data.append(evaporation_w)
+                                
+                            elif variable_name == 'wind speed u':
+                                u10_w = f.variables['ugrd10m'][:][0]  #unit = m/s
+                                all_data.append(u10_w)
+                                
+                            elif variable_name == 'wind speed v':
+                                v10_w = f.variables['vgrd10m'][:][0]  #unit = m/s
+                                all_data.append(v10_w)
+                                
+                            elif variable_name == 'pblh':
+                                pblh_w = f.variables['hpbl'][:][0]    #unit = m
+                                all_data.append(pblh_w)
+                                
+                            elif variable_name == 'precipitation':
+                                precipitation_w = f.variables['tprcp'][:][0]  #unit = kg/m^2
+                                all_data.append(precipitation_w)
                             
                             time_str = f"{year}{month}{day}_{filelist[i].replace('aqm.t','').replace('.phy.f0','')}"
                             times.append(time_str)
-                            all_data.append(aod)
+                            
         
         
         'step2.Create xarray Dataset'
         ds = xr.Dataset(
             {
-                'aod': (['time', 'lat', 'lon'], np.stack(all_data)),
+                variable_name: (['time', 'lat', 'lon'], np.stack(all_data)),
                 'lat': (['lat', 'lon'], lat),
                 'lon': (['lat', 'lon'], lon)
             },
@@ -115,7 +139,52 @@ class Load_Meteo:
         )
         return ds
 
+class Load_Chem:
+    def extract_data(chem_folder,variable_name, year_list, month_list, day_list, size_lat_ufs, size_lon_ufs):
+        "Step 1: Extract data and return as xarray Dataset"
+        all_data = []
+        times = []
+        
+        for year in year_list:
+            for month in month_list:
+                for day in day_list:
+                    dir_data = chem_folder+f'/{year}/{year}{month}/{year}{month}{day}/'
+                    pattern = 'aqm.t12z.chem_sfc.nc'
+                    filename = dir_data+pattern
+                    f = Dataset(filename,'r')
+    
+                    for i in range(24):
+ 
+                        lat = f.variables['lat'][0,:,:]
+                        lon = f.variables['lon'][0,:,:] - 360
+                        
+                        if variable_name == 'pm25':                            
+                            pm25_w = f.variables['PM25_TOT'][i,0,:,:]
+                            all_data.append(pm25_w)
+                            
+                        elif variable_name == 'o3':                          
+                            o3_w = f.variables['o3'][i,0,:,:] 
+                            all_data.append(o3_w)
+                            
 
+                        
+                        time_str = f"{year}{month}{day}_{filename.replace('aqm.t','').replace('.chem_sfc','')}"
+                        times.append(time_str)
+                            
+        
+        
+        'step2.Create xarray Dataset'
+        ds = xr.Dataset(
+            {
+                variable_name: (['time', 'lat', 'lon'], np.stack(all_data)),
+                'lat': (['lat', 'lon'], lat),
+                'lon': (['lat', 'lon'], lon)
+            },
+            coords={
+                'time': times
+            }
+        )
+        return ds
 
 
 
